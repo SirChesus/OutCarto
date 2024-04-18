30b65168bbcef7d7f06b8a0d67e3998a099d097e
SirChesus <157728001+SirChesus@users.noreply.github.com>
2024-04-18 10:57:14 -0700

    Update cnnMain.py
    comments

--- cnnMain.py
+++ cnnMain.py
@@ -2,6 +2,7 @@ from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activati
 from keras.models import Sequential
 from imageprocess import train_generator, validation_generator
 
+#create model, add layers & activation fuct.
 model = Sequential()
 
 model.add(Conv2D(32,(3,3), input_shape=(224,224, 3)))
@@ -29,5 +30,7 @@ model.add(Activation('relu'))
 model.add(Dense(4))
 model.add(Activation('softmax'))
 
+#complie with loss funct, optimizer, & metric
+#fit the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
 model.fit_generator(train_generator, epochs = 10, validation_data= validation_generator)

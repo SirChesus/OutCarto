aa106cddaa1b1aedfff43562ca517176a8fc18bd
SirChesus <157728001+SirChesus@users.noreply.github.com>
2024-04-18 10:26:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from imageprocess import *
 from cnnMain import *
 
+# printing predictions 
 for i in range(len(examples_generator.filenames)):
   print("File Name: ",str(examples_generator.filenames[i]))
   print("Label: ", list(train_generator.class_indices.items())[i][0])

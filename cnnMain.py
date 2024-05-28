72435492e73ee21f02b1ec125ed8e40e2b820b18
Kevin Sandberg <Kevinsandb3607@student.lvusd.org>
2024-05-27 18:06:50 -0700

    added displays for different images in dataset

--- cnnMain.py
+++ cnnMain.py
@@ -9,15 +9,11 @@ model.add(Conv2D(32,(3,3), input_shape=(50,50, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(124, (3,3)))
+model.add(Conv2D(32, (3,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-<<<<<<< Updated upstream
-model.add(Conv2D(64,(3,3)))
-=======
-model.add(Conv2D(124, (3,3)))
->>>>>>> Stashed changes
+model.add(Conv2D(32, (3,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
@@ -27,21 +23,14 @@ model.add(Dropout(.25))
 
 model.add(Flatten())
 
-model.add(Dense(122))
+model.add(Dense(32))
 model.add(Activation('relu'))
 
-model.add(Dense(122))
+model.add(Dense(32))
 model.add(Activation('relu'))
 
-<<<<<<< Updated upstream
-model.add(Dense(4))
-model.add(Activation('softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
-=======
 model.add(Dense(8))
 model.add(Activation('softmax'))
 
-model.compile(loss=keras.losses.CategoricalFocalCrossentropy(), optimizer=keras.optimizers.Adam(learning_rate= 1e-4, weight_decay=0.002), metrics = ['accuracy', keras.metrics.TopKCategoricalAccuracy(k=3)])
->>>>>>> Stashed changes
-model.fit(train_generator, epochs = 10, validation_data= validation_generator)
+model.compile(loss=keras.losses.CategoricalFocalCrossentropy(), optimizer=keras.optimizers.Adam(learning_rate= 1e-2, weight_decay=0.002), metrics = ['accuracy', keras.metrics.TopKCategoricalAccuracy(k=3)])
+model.fit(train_generator, epochs = 3, validation_data= validation_generator)
--- predictions.py
+++ predictions.py
@@ -1,23 +1,83 @@
 from cnnMain import model
 import imageprocess
+import keras
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib import animation
+import numpy as np
+from matplotlib.widgets import Button
+
+
+
 #-----------------------------------------------------------------------
 #examples_datagen = imageprocess.ImageDataGenerator(rescale=1./255)
 #examples_generator = examples_datagen.flow_from_directory(imageprocess.PATH, target_size=(224,224), classes=['examples'], shuffle=False)
 example_datagen = imageprocess.ImageDataGenerator(rescale=1./255)
-examples_generator = example_datagen.flow_from_directory(imageprocess.test_dir, target_size=(50,50), shuffle=False)
+#examples_generator = example_datagen.flow_from_directory(imageprocess.PATH, target_size=(50,50), shuffle=False)
+examples_generator = keras.utils.image_dataset_from_directory(imageprocess.test_dir, image_size=(50,50), shuffle=False)
 
 
-examples_generator.reset()
-pred = model.predict(examples_generator)
+# Load the image using Matplotlib
+#image = mpimg.imread(examples_generator.file_paths[0])
+# Display the image
+#plt.imshow(image)
+#plt.axis('off')  # Turn off axis
+#plt.show()
 
-predicted_classes = []
-print('---- ', model.predict_on_batch(examples_generator[0]))
+#for y in examples_generator.file_paths:
+#  myobj = plt.imshow(mpimg.imread(y))
+#  myobj.set_data(y)  
 
-for p in pred:
-  predicted_class = -1
-  max = -1
-  for i in range(len(p)):
-    if p[i] > max:
-      max = p[i]
-      predicted_class = i
-  predicted_classes.append(predicted_class)
+images = []
+for i in examples_generator.file_paths:
+  images.append(mpimg.imread(i))
+
+# Set up the figure and axis
+fig, ax = plt.subplots()
+img = ax.imshow(images[0], cmap='gray')  # Initial image
+# Initial image index
+current_index = 0
+
+# Function to update the displayed image
+def update_image(index):
+    img.set_data(images[index])
+    ax.set_title(f'Image {index + 1}')
+    plt.draw()
+
+# Callback function for the button
+def next_image(event):
+    global current_index
+    current_index = (current_index + 1) % len(images)
+    update_image(current_index)
+
+# Set up the figure and axis
+fig, ax = plt.subplots()
+img = ax.imshow(images[current_index], cmap='gray')  # Initial image
+ax.set_title(f'Image {current_index + 1}')
+
+# Create the "Next" button
+ax_button = plt.axes([0.81, 0.05, 0.1, 0.075])
+btn_next = Button(ax_button, 'Next')
+btn_next.on_clicked(next_image)
+
+# Display the plot
+plt.show()
+
+
+#print(len(examples_generator))
+#for i in range(len(examples_generator.file_paths)):
+  #print("file name is " + examples_generator.file_paths[i])
+
+#examples_generator.reset()
+#pred = model.predict(examples_generator)
+
+#predicted_classes = []
+
+#for p in pred:
+  #predicted_class = -1
+  #max = -1
+  #for i in range(len(p)):
+    #if p[i] > max:
+      #max = p[i]
+      #predicted_class = i
+  #predicted_classes.append(predicted_class)
